9893937131fc8fee6fa9d22b1bbc85c5eb417426
MasumBilla19 <masumiraj0218@gmail.com>
2022-02-22 10:04:05 +0600

    update

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("open").addEventListener('click', function() {
-    const closeBtn = document.getElementsByClassName('.sidebar_area');
-    closeBtn.style.display = "flex";
-});
